680d73bd9fd037ad231c0fd8963c7a607a0e1d6b
DESKTOP-IQ7PMTJ\ASUS <lixiaodantogo@163.com>
2019-04-16 13:26:42 +0800

    '1，2'

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
@@ -5,13 +5,16 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice01ClipRectView extends View {
+    private static final String TAG = "Practice01ClipRectView";
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
 
@@ -38,6 +41,12 @@ public class Practice01ClipRectView extends View {
         int left = (getWidth() - bitmap.getWidth()) / 2;
         int top = (getHeight() - bitmap.getHeight()) / 2;
 
+        Log.e(TAG, "left = " + left + " top = " + top
+                + " getWidth() = " + getWidth() + " getHeight() = " + getHeight());
+
+        canvas.save();
+        canvas.clipRect(new Rect(left + 50, top + 50, left + 300, top + 200));
         canvas.drawBitmap(bitmap, left, top, paint);
+        canvas.restore();
     }
 }
